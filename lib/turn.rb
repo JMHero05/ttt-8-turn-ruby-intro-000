00df7234b44e5bd30eb1998535d67354e8611a48
Jake Mills <jake.mills.05@gmail.com>
2020-06-23 18:22:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index == (0..8) && (board[index] != "X" || board[index] != "Y")
+  if board[index] == (0..8) && (board[index] != "X" || board[index] != "Y")
     true
   else
   end
